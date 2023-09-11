2ff102d4f9dd2d7bcfb1c6aa80624317db6bedb2
shadowjerry <370746534kjc@gmail.com>
2023-09-10 23:05:17 -0500

    stable solution

--- start-all.sh
+++ start-all.sh
@@ -1,4 +1,5 @@
 #!/bin/bash
+(docker rm cs511p1-main-1 cs511p1-worker2-1 cs511p1-worker1-1 || true) && \
 docker build -t cs511p1-common -f cs511p1-common.Dockerfile . && \
     docker build -t cs511p1-main -f cs511p1-main.Dockerfile . && \
     docker build -t cs511p1-worker -f cs511p1-worker.Dockerfile . && \
